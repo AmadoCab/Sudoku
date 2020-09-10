a45cb25725f59b4677f3794ca5b6e95e8442c09e
Macbook <Macbook@MacBook-Pro-de-Macbook.local>
2020-09-09 18:33:44 -0600

    to poo

---
+++ Sudoku.py
@@ -0,0 +1,90 @@
+guide = """/ 1 2 3 4 5 6 7 8 9
+A
+B
+C
+D
+E
+F
+G
+H
+I
+"""
+
+class Sudoku:
+    # Propiedades
+    grid_inicial = [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    ]
+    completed = False
+    correct = False
+
+    def __init__(self, cadena):
+        self.cadena = cadena
+        cadena = str(cadena)
+        if len(cadena) == 81:
+            for i in range(9):
+                for j in range(9):
+                    self.grid_inicial[i][j] == int(cadena[i*9+j])
+        else:
+            raise Exception('LengthError: La longitud de la cadena debe ser de 81 caracteres')
+
+    # Metodos
+    def posibilities(self):
+        """Coloca la posibilidad de valor en cada casilla que no tenga un valor 
+        previamente no tenga un valor definido"""
+        for i in range(9):
+            for j in range(9):
+                if self.grid_inicial[i][j] == 0:
+                    self.grid_inicial[i][j] = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+    def is_complete(self):
+        """Verifica si en el sudoku los valores colocados son todos enteros"""
+        for i in range(9):
+            for j in range(9):
+                if type(self.grid_inicial[i][j]) == type([]):
+                    self.completed = False
+                    return 0
+        self.completed = True
+        return 1
+
+    def make_string(self):
+        cadena_prov = ''
+        for i in range(9):
+            for j in range(9):
+                if type(self.grid_inicial[i][j]) == type(1):
+                    cadena_prov += str(self.grid_inicial[i][j])
+                elif type(self.grid_inicial[i][j]) == type([]):
+                    cadena_prov += '0'
+        self.cadena = cadena_prov
+
+def imprimir(cadena):
+    if type(cadena) == type('2'):
+        if len(cadena) != 81:
+            raise Exception('LengthError: La longitud de la cadena debe ser de 81 caracteres')
+        print(f"""\t{cadena[0]} {cadena[1]} {cadena[2]} {cadena[3]} {cadena[4]} {cadena[5]} {cadena[6]} {cadena[7]} {cadena[8]}
+        {cadena[9]} {cadena[10]} {cadena[11]} {cadena[12]} {cadena[13]} {cadena[14]} {cadena[15]} {cadena[16]} {cadena[17]}
+        {cadena[18]} {cadena[19]} {cadena[20]} {cadena[21]} {cadena[22]} {cadena[23]} {cadena[24]} {cadena[25]} {cadena[26]}
+        {cadena[27]} {cadena[28]} {cadena[29]} {cadena[30]} {cadena[31]} {cadena[32]} {cadena[33]} {cadena[34]} {cadena[35]}
+        {cadena[36]} {cadena[37]} {cadena[38]} {cadena[39]} {cadena[40]} {cadena[41]} {cadena[42]} {cadena[43]} {cadena[44]}
+        {cadena[45]} {cadena[46]} {cadena[47]} {cadena[48]} {cadena[49]} {cadena[50]} {cadena[51]} {cadena[52]} {cadena[53]}
+        {cadena[54]} {cadena[55]} {cadena[56]} {cadena[57]} {cadena[58]} {cadena[59]} {cadena[60]} {cadena[61]} {cadena[62]}
+        {cadena[63]} {cadena[64]} {cadena[65]} {cadena[66]} {cadena[67]} {cadena[68]} {cadena[69]} {cadena[70]} {cadena[71]}
+        {cadena[72]} {cadena[73]} {cadena[74]} {cadena[75]} {cadena[76]} {cadena[77]} {cadena[78]} {cadena[79]} {cadena[80]}""")
+    else:
+        raise TypeError
+
+if __name__ == "__main__":
+    s1 = '005407000200065089603002704032058000806000003509043678300789040400536000007120806'
+    sudoku = Sudoku(s1)
+    print(type(sudoku))
+    imprimir(sudoku.cadena)
+
+#
---
+++ main_func.py
@@ -0,0 +1,177 @@
+# Es la matriz en la que se almacena el sudoku
+GRID_INICIAL = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0]
+]
+
+# Es la guía para ubicarse en el sudoku
+GUIDE = """/ 1 2 3 4 5 6 7 8 9
+A
+B
+C
+D
+E
+F
+G
+H
+I
+"""
+
+# Imprime el sudoku un poco más lindo para que lo vea
+def imprimir(matriz):
+    for i in range(9):
+        print(matriz[i])
+    print('\n')
+
+# Genera a partir de una cadena la matriz del sudoku
+def generate_sudoku(cadena):
+    cadena = str(cadena)
+    while len(cadena) != 81:
+        print('La longitud de la cadena a ingresar debe ser de 81 caracteres')
+        print('intentelo una vez más.')
+        cadena = input('Reingrese el sudoku: ')
+    for i in range(9):
+        for j in range(9):
+            GRID_INICIAL[i][j] = int(cadena[i*9+j])
+    imprimir(GRID_INICIAL)
+
+# Crea un sudoku sacado de internet
+def make_from_i():
+    pass
+
+# Crea un soduku a partir de una cadena continua de numeros
+def make_from_str():
+    print('Ingrese una cadena de la configuración de su sudoku')
+    print('los valores que desconozca marquelos con un 0, no utilice espacios')
+    print('los valores se colocaran por fila, correspondiendo los primeros')
+    print('nueve a los valores Ai, i=1,2,...,9. y sucesivamente.')
+    entrada = input('Ingrese su sudoku: ')
+    generate_sudoku(entrada)
+
+# Sustituye cada valor nulo por una lista con todas las posibilidades
+def posibilities():
+    for i in range(9):
+        for j in range(9):
+            if GRID_INICIAL[i][j] == 0:
+                GRID_INICIAL[i][j] = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+# Elimina las posibilidades de las listas horizontalmente
+def solve_horizontal(value, i, j):
+    for x in range(9):
+        if type(GRID_INICIAL[i][x]) == type([]):
+            if GRID_INICIAL[i][x].count(value) != 0:
+                GRID_INICIAL[i][x].remove(value)
+
+# Elimina las posibilidades de las listas verticalmente
+def solve_vertical(value, i, j):
+    for y in range(9):
+        if type(GRID_INICIAL[y][j]) == type([]):
+            if GRID_INICIAL[y][j].count(value) != 0:
+                GRID_INICIAL[y][j].remove(value)
+
+# Elimina las posibilidades de las listas de cada cuadrante
+def solve_square(value, i, j):
+    if 0<=i<3 and 0<=j<3:
+        for x in range(0,3):
+            for y in range(0,3):
+                if type(GRID_INICIAL[y][x]) == type([]):
+                    if GRID_INICIAL[y][x].count(value) != 0:
+                        GRID_INICIAL[y][x].remove(value)
+    elif 3<=i<6 and 0<=j<3:
+        for x in range(0,3):
+            for y in range(3,6):
+                if type(GRID_INICIAL[y][x]) == type([]):
+                    if GRID_INICIAL[y][x].count(value) != 0:
+                        GRID_INICIAL[y][x].remove(value)
+    elif 6<=i<9 and 0<=j<3:
+        for x in range(0,3):
+            for y in range(6,9):
+                if type(GRID_INICIAL[y][x]) == type([]):
+                    if GRID_INICIAL[y][x].count(value) != 0:
+                        GRID_INICIAL[y][x].remove(value)
+    elif 0<=i<3 and 3<=j<6:
+        for x in range(3,6):
+            for y in range(0,3):
+                if type(GRID_INICIAL[y][x]) == type([]):
+                    if GRID_INICIAL[y][x].count(value) != 0:
+                        GRID_INICIAL[y][x].remove(value)
+    elif 3<=i<6 and 3<=j<6:
+        for x in range(3,6):
+            for y in range(3,6):
+                if type(GRID_INICIAL[y][x]) == type([]):
+                    if GRID_INICIAL[y][x].count(value) != 0:
+                        GRID_INICIAL[y][x].remove(value)
+    elif 6<=i<9 and 3<=j<6:
+        for x in range(3,6):
+            for y in range(6,9):
+                if type(GRID_INICIAL[y][x]) == type([]):
+                    if GRID_INICIAL[y][x].count(value) != 0:
+                        GRID_INICIAL[y][x].remove(value)
+    elif 0<=i<3 and 6<=j<9:
+        for x in range(6,9):
+            for y in range(0,3):
+                if type(GRID_INICIAL[y][x]) == type([]):
+                    if GRID_INICIAL[y][x].count(value) != 0:
+                        GRID_INICIAL[y][x].remove(value)
+    elif 3<=i<6 and 6<=j<9:
+        for x in range(6,9):
+            for y in range(3,6):
+                if type(GRID_INICIAL[y][x]) == type([]):
+                    if GRID_INICIAL[y][x].count(value) != 0:
+                        GRID_INICIAL[y][x].remove(value)
+    elif 6<=i<9 and 6<=j<9:
+        for x in range(6,9):
+            for y in range(6,9):
+                if type(GRID_INICIAL[y][x]) == type([]):
+                    if GRID_INICIAL[y][x].count(value) != 0:
+                        GRID_INICIAL[y][x].remove(value)
+
+# Aplica cada una de las funciones solve_...
+def solve():
+    for i in range(9):
+        for j in range(9):
+            if type(GRID_INICIAL[i][j]) == type(6):
+                solve_horizontal(GRID_INICIAL[i][j], i, j)
+                solve_vertical(GRID_INICIAL[i][j], i, j)
+                solve_square(GRID_INICIAL[i][j], i, j)
+                substitute()
+                imprimir(GRID_INICIAL)
+            else:
+                pass
+
+# Sustituye por un entero una lista de posibilidades cuando solo hay una posibilidad
+def substitute():
+    for i in range(9):
+        for j in range(9):
+            if type(GRID_INICIAL[i][j]) == type([]):
+                if len(GRID_INICIAL[i][j]) == 1:
+                    GRID_INICIAL[i][j] = GRID_INICIAL[i][j][0]
+                else:
+                    pass
+            else:
+                pass
+
+# Dice si el sudoku ya está solucionado o aun no
+def is_solved():
+    for i in range(9):
+        for j in range(9):
+            if GRID_INICIAL[i][j] == type([]):
+                return False
+    return True
+
+# Provisionalmente los valores que uso para testear el programa
+s1 = '005407000200065089603002704032058000806000003509043678300789040400536000007120806'
+generate_sudoku(s1)
+posibilities()
+
+for _ in range(3):
+    solve()
+
+print(GUIDE)
